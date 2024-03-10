7ed071eec11708d0e3ba02c5a143789b6040810b
Aditya Alshi <alshiaditya55@gmail.com>
2024-03-10 14:19:50 +0530

    Added one project

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="all--projects">
       <h1>Projects</h1>
       <a
+        className="ject--link--spnb"
         href="https://jectcommunitybank.onrender.com/"
         rel="noopener noreferrer"
         target="_blank"
@@ -10,6 +11,17 @@ export default function Home() {
         <img
           className="project-thumbnails"
           src="jecthome.jpg"
+          alt="project-thumbnail"
+        />
+      </a>
+      <a
+        href="https://65ed6b0093097c3f65fe7df7--shop-whop.netlify.app/"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          className="project-thumbnails"
+          src="ShopWhop.jpg"
           alt="project-thumbnail"
         />
       </a>
